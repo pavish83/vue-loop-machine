a7c38118162bbd6d270ee31a92f41ffbd80ae1d4
pavish83 <pavish83@gmail.com>
2022-05-15 07:51:10 +0300

    css background

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,6 @@ body {
   color: #2c3e50;
   height: 100vh;
   display: flex;
-  background: linear-gradient(90deg, rgba(255,214,142,1) 0%, rgba(254,183,70,1) 35%, rgba(249,149,7,0.6446953781512605) 100%);
 }
 
 </style>
